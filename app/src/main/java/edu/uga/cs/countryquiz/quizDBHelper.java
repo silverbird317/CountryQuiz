e34573eec85505aa5a7c40ed209e49ed57a829d1
RoshniHariharan <roshnihariharan01@gmail.com>
2023-04-06 19:43:30 -0400

    original helper class incorrect, created new one

---
+++ app/src/main/java/edu/uga/cs/countryquiz/quizDBHelper.java
@@ -0,0 +1,50 @@
+package edu.uga.cs.countryquiz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class quizDBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "countryQuizDB.db";
+    private static final int DB_VERSION = 1;
+
+    //private reference to single instance
+    private static quizDBHelper helperInstance;
+
+    //private constructor
+    private quizDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    //public method to access the instance
+    public static synchronized quizDBHelper getInstance(Context context) {
+        if(helperInstance == null) {
+            helperInstance = new quizDBHelper(context.getApplicationContext());
+        }
+        return helperInstance;
+    }
+    //defining column names as Java constants
+    public static final String TABLE_COUNTRYLIST = "countryList";
+    public static final String COUNTRYLIST_COLUMN_CID = "cid";
+    public static final String COUNTRYLIST_COLUMN_COUNTRY = "country";
+    public static final String COUNTRYLIST_COLUMN_CONTINENT = "continent";
+
+    private static final String CREATE_COUNTRYLIST =
+            "create table" + TABLE_COUNTRYLIST + " ("
+            + COUNTRYLIST_COLUMN_CID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COUNTRYLIST_COLUMN_COUNTRY + " TEXT, "
+            + COUNTRYLIST_COLUMN_CONTINENT + " TEXT"
+            + ")";
+
+
+    //creates database
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL((CREATE_COUNTRYLIST));
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_COUNTRYLIST);
+        onCreate(db);
+    }
+}
