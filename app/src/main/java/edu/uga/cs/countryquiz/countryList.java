dba73a4d3ee39d1e88ded125d4a1c331cecae3df
RoshniHariharan <roshnihariharan01@gmail.com>
2023-04-06 19:48:43 -0400

    pojo class to represent quiz content

---
+++ app/src/main/java/edu/uga/cs/countryquiz/countryList.java
@@ -0,0 +1,14 @@
+package edu.uga.cs.countryquiz;
+
+public class countryList {
+    private long cid;
+    private String country;
+    private String continent;
+
+    public countryList(){
+        this.cid = -1;
+        this.country = null;
+        this.continent = null;
+    }
+}
+
